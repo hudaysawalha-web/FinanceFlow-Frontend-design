48c45a2e5e2eb338844c122c41829b97dfc1793e
hudaysawalha-web <hudasawalha3@gmail.com>
2026-08-04 17:11:48 +0300

    feat: implement theme toggle functionality and add Font Awesome for icons

---
+++ main.js
@@ -0,0 +1,30 @@
+const themetoggle = document.querySelector("#theme-toggle");
+const html = document.documentElement;
+
+
+function applyTheme(theme) {
+    if (theme == 'green') {
+        html.classList.add('green');
+        document.getElementById('theme-icon').innerHTML = '<i class="fa-solid fa-brush text-2xl text-blue "></i>';
+    } else {
+        html.classList.remove('green');
+        document.getElementById('theme-icon').innerHTML = '<i class="fa-solid fa-brush text-2xl text-blue "></i>';
+    }
+}
+
+themetoggle.addEventListener('click', () => {
+    const newTheme = html.classList.contains('green') ? 'blue' : 'green';
+    localStorage.setItem('theme',newTheme);
+    applyTheme(newTheme);
+})
+
+const savedTheme= localStorage.getItem('theme') || 'blue';
+applyTheme(savedTheme);
+
+const menuToggle = document.querySelector('[menu-toggle]');
+const menu = document.querySelector('[mobile-menu-list]');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
+    menuToggle.classList.toggle('menu-open');
+});
